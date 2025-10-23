669f5a036ad0922adade9844866df0d1cba95c68
odenilsonmarques <53958086+odenilsonmarques@users.noreply.github.com>
2025-10-23 12:52:57 +0000

    Utilizando  recursividade para representar o movimento das peças

--- xadrez.c
+++ xadrez.c
@@ -1,58 +1,81 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - novato
+// Desafio de Xadrez - mestre
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+// Função recursiva para representar o movimento da Torre
+void moveTorre(int movimentoTorre)
+{
+    if (movimentoTorre == 0)
+    {
+        return;
+    }
+    else
+    {
+        moveTorre(movimentoTorre - 1);
+        printf("CASA %d: DIREITA \n", movimentoTorre);
+    }
+}
+
+// Funcão recursiva para representar o movimento do Bispo
+void moveBispo(int movimentoBispo)
+{
+    if (movimentoBispo == 0)
+    {
+        return;
+    }
+    else
+    {
+        moveBispo(movimentoBispo - 1);
+        printf("CASA %d: CIMA, DIREITA\n", movimentoBispo);
+    }
+}
+
+// Funcão recursiva para representar o movimento da Rainha
+void moveRainha(int movimentoRainha)
+{
+    if (movimentoRainha == 0)
+    {
+        return;
+    }
+    else
+    {
+        moveRainha(movimentoRainha - 1);
+        printf("CASA %d: ESQUERDA\n", movimentoRainha);
+    }
+}
+
 int main()
 {
-
-    // ----------------MOVIMENTANDO A TORRE USANDO O LAÇO FOR---------------------
+    // ----------------MOVIMENTANDO A TORRE USANDO RECURSIVIDADE---------------------
     printf("------ MOVIMENTO DA TORRE ------\n");
     printf("A TORRE SE MOVE 5 CASAS PARA A DIREITA \n");
 
-    for (int t = 1; t <= 5; t++)
-    {
-        printf("Casa %d: Direita \n", t);
-    }
-
+    moveTorre(5);
     printf("A TORRE TERMINOU O MOVIMENTO!\n\n");
 
 
-    // ---------------- MOVIMENTANDO O BISPO USANDO O LAÇO WHILE------------------
+
+    // ---------------- MOVIMENTANDO O BISPO USANDO RECURSIVIDADE------------------
     printf("------ MOVIMENTO DO BISPO ------\n");
     printf("O BISPO SE MOVE 5 CASAS NA DIAGONAL PARA CIMA E A DIREITA \n");
 
-    // variavel para controlar o laço(movimentação do Bispo)
-    int b = 1;
-
-    while (b <= 5)
-    {
-        printf("Casa %d: Cima, Direita\n", b);
-        b++;
-    }
+    moveBispo(5);
     printf("O BISPO TERMINOU O MOVIMENTO!\n\n");
 
 
 
-    //----------------MOVIMENTANDO A RAINHA USANDO LAÇO DO WHILE-------------------
+    //----------------MOVIMENTANDO A RAINHA USANDO RECURSIVIDADE-------------------
     printf("------ MOVIMENTO DA RAINHA ------\n");
     printf("A RAINHA SE MOVE 8 CASAS PARA A ESQUERDA \n");
 
-     // variavel para controlar o laço(movimentação da Rainha)
-     int r = 1;
-
-     do{
-        printf("Casa %d: Esquerda\n", r);
-        r++;
-     }
-     while(r <= 8);
-
-     printf("A RAINHA TEMINOU O MOVIMENTO \n\n");
+    moveRainha(8);
+    printf("A RAINHA TEMINOU O MOVIMENTO \n\n");
 
 
 
-    //----------------MOVIMENTO DO CAVALO USANDO OS LAÇOS WHILE E FOR-------------------
+    //----------------MOVIMENTO DO CAVALO USANDO OS LOOPS ANINHADOS COM MULTIPLAS VARIAVEIS-------------------
     printf("------ MOVIMENTO DO CAVALO ------\n");
     printf("O CAVALO SE MOVE 2 CASAS PARA BAIXO E 1 PARA A ESQUERDA \n");
     int c = 0;
@@ -61,12 +84,12 @@ int main()
     while (c < 1)
     {
         // Primeiro, o cavalo anda 2 casas para baixo
-        for (int j = 1; j <= 2; j++)
+        for (int i = 1, j = 2; i <= j; i++)
         {
-            printf("CASA %d: PARA BAIXO\n", j);
+            printf("CASA %d: PARA CIMA\n", i);
         }
 
-           printf("MOVENDO UMA CASA PARA A ESQUERDA ESQUERDA (1/1)\n");
+        printf("MOVENDO UMA CASA PARA A DIREITA ESQUERDA (1/1)\n");
         c++;
     }
 
